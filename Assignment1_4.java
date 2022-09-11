eadffdfa9366631f699c71eb82c3896c73d9a21a
Sanju Juttannavar <jpsanju2002@gmail.com>
2022-09-11 19:35:08 +0530

    changed the content of the file

--- Assignment1_4.java
+++ Assignment1_4.java
@@ -1,13 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 
 public class Assignment1_4 {
     public static void main(String[] args) {
         try {
-            BufferedReader reader=new BufferedReader(new FileReader("Alphabets.txt"));
+          
                         
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
            System.out.println(e);
         }
         
